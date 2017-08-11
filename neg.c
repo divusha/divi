e8dea62d7444958a14f0ffb619bcf7ce648a8461
divusha <30368581+divusha@users.noreply.github.com>
2017-08-11 17:05:37 +0530

    neg.c

---
+++ neg.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter the num");
+scanf("%d",&num);
+if(n>0)
+{
+printf("The num is positive");
+}
+else if(n<0)
+{
+printf("The num is negative");
+}
+else
+{
+printf("The num is zero");
+}
+return 0;
+}
